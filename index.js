e15058a3e7c7c85a7c6b179c6e83bbda0ff8a94d
Michelle :) <mitchfibi@gmail.com>
2021-10-21 16:53:05 +0300

    First Commit. will add more features tomorrow.

---
+++ index.js
@@ -0,0 +1,86 @@
+const hexInput = document.getElementById('hexInput');
+const inputColor = document.getElementById('inputColor');
+const alteredColor = document.getElementById('alteredColor');
+const alteredColorText = document.getElementById('alteredColorText');
+const slider = document.getElementById('slider');
+const sliderText = document.getElementById('sliderText');
+
+hexInput.addEventListener('keyup', () => {
+    const hex = hexInput.value;
+    if (!isValidHex(hex)) return;
+    const strippedHex = hex.replace('#', '');
+    inputColor.style.backgroundColor = "#" + strippedHex;
+})
+
+//checking if hex code is valid 
+const isValidHex = (hex) => {
+    if (!hex) return false;
+    const strippedHex = hex.replace('#', '');
+    return strippedHex.length === 3 || strippedHex.length === 6;
+}
+
+//convert Hex to RGB
+const convertHexToRGB = (hex) => {
+    if (!isValidHex(hex)) return null;
+
+    let strippedHex = hex.replace('#', '');
+    //double the numbers
+    if (strippedHex.length === 3) {
+        strippedHex = strippedHex[0] + strippedHex[0] +
+            strippedHex[1] + strippedHex[1] +
+            strippedHex[2] + strippedHex[2];
+    }
+    //parse to individual substrings and assign to rgb 
+    const r = parseInt(strippedHex.substring(0, 2), 16);
+    const g = parseInt(strippedHex.substring(2, 4), 16);
+    const b = parseInt(strippedHex.substring(4, 6), 16);
+
+    return { r, g, b };
+}
+
+//convert rgb to hex
+const convertRGBToHex = (r, g, b) => {
+    const firstPair = ("0" + r.toString(16)).slice(-2); // add zero to result and get the last two digits 
+    const secondPair = ("0" + g.toString(16)).slice(-2);
+    const thirdPair = ("0" + b.toString(16)).slice(-2);
+
+    const hex = "#" + firstPair + secondPair + thirdPair;
+    return hex;
+}
+
+
+//alter color by percentage
+const alterColor = (hex, percentage) => {
+    const { r, g, b } = convertHexToRGB(hex);
+
+    const amount = Math.floor((percentage / 100) * 255); // increase each r,g,b value by appropriate amount (percentage of 255)
+
+    const newR = increaseWithin0To255(r, amount);
+    const newG = increaseWithin0To255(g, amount);
+    const newB = increaseWithin0To255(b, amount)
+    return convertRGBToHex(newR, newG, newB);
+}
+
+
+//ensure it stays within range 
+const increaseWithin0To255 = (hex, amount) => {
+    // const newHex = hex + amount;
+    // if(newHex > 255) return 255;
+    // if(newHex < 0) return 0;
+    // return newHex;
+    return Math.min(255, Math.max(0, hex + amount));
+}
+
+alterColor("fff", 10)
+    //display slider percentage.
+slider.addEventListener('input', () => {
+    if (!isValidHex(hexInput.value)) return;
+
+    sliderText.textContent = `${slider.value}%`;
+
+    //get the altered hex value
+    const alteredHex = alterColor(hexInput.value, slider.value);
+    alteredColor.style.backgroundColor = alteredHex;
+    alteredColorText.innerText = `Altered Color ${alteredHex}`;
+    //update the altered color
+})
